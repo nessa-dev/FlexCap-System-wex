56897f835b762a4c7d0f2278e2118a0d7c8d20d9
nessa-dev <vanessacsantosdev@gmail.com>
2025-08-13 12:07:11 -0300

    atualização

--- FlexCap.Web/Models/Login/LoginViewModel.cs
+++ FlexCap.Web/Models/Login/LoginViewModel.cs
@@ -1,26 +1,26 @@
-﻿using FlexCap.Web.Models.Login;
-using Microsoft.AspNetCore.Mvc;
-
-namespace FlexCap.Web.Controllers.Login
-{
-    public class LoginController : Controller
-    {
-        [HttpGet]
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Login(LoginViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                return RedirectToAction("Index", "Home");
-            }
-
-            return View("Index");
-        }
-    }
-
-}
+﻿// using FlexCap.Web.Models.Login;
+// using Microsoft.AspNetCore.Mvc;
+//
+// namespace FlexCap.Web.Controllers.Login
+// {
+//     public class LoginController : Controller
+//     {
+//         [HttpGet]
+//         public IActionResult Index()
+//         {
+//             return View();
+//         }
+//
+//         [HttpPost]
+//         public IActionResult Login(LoginViewModel model)
+//         {
+//             if (ModelState.IsValid)
+//             {
+//                 return RedirectToAction("Index", "Home");
+//             }
+//
+//             return View("Index");
+//         }
+//     }
+//
+// }
